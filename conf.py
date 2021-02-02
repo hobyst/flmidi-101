5aed77c915ccdb17caeb3b6201871bc266f1ee4e
hobyst <48522412+hobyst@users.noreply.github.com>
2021-02-02 14:34:22 +0100

    Update main.yml and conf.py

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Hobyst & flmidi-101 contributors'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.githubpages', 'sphinx.ext.autodoc', 'sphinx_typlog_theme']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.githubpages', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
